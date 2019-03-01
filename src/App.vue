dec33f0ea68d269c28ac3a7ccd2ca651b02082a3
John Ingle <jingle1000@gmail.com>
2019-03-01 15:45:16 -0500

    Modify App to allow eventbus delete array Items. Doesn't work currenly

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,39 @@
     <Menu />
     <v-content>
       <v-container>
-  <Skills/>
+        <Skills :skillArray="skills" @add-skill="addSkill($event)"></Skills>
       </v-container>
-    
     </v-content>
   </v-app>
 </template>
 
 <script>
-import Menu from './components/Menu'
-import Skills from './components/Skills'
-
-export default {
-  name: 'App',
-  components: {
-    Menu,
-    Skills
-  },
-  data () {
-    return {
-      //
+  import Menu from './components/Menu'
+  import Skills from './components/Skills'
+  import {EventBus} from './eventbus.js'
+  export default {
+    name: 'App',
+    components: {
+      Menu,
+      Skills
+    },
+    data() {
+      return {
+        maxSkills: 10,
+        skills: ["Skill Placeholder", "test"],
+        toDel: 0
+      }
+    },
+    methods: {
+      addSkill(skill) {
+        this.skills.push(skill);
+      },
+    },
+    mounted() {
+      EventBus.$on('delete-skill', (s) => {
+        console.log(this.skills.indexOf(s));
+        this.skills.pop();
+      });
     }
   }
-}
 </script>
